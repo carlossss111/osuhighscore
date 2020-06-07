1630a683de35bbab8e6dd76a41eb2ce24e9d770b
LinuxDaniel <danielrobinson1112@gmail.com>
2020-06-07 17:22:25 +0100

    Added yourcodeshere.h

---
+++ yourcodeshere.h
@@ -0,0 +1,6 @@
+//Rename this file to "wificodes.h" and add your personal details below:
+char ssid[] = "YOUR SSID HERE";
+char pass[] = "YOUR PASSWORD HERE";
+//Requires the key for the legacy api. It is simpler and better suited while it still runs.
+//Keys can be obtained from  https://osu.ppy.sh/p/api/
+String apikey = "YOUR API KEY HERE";
